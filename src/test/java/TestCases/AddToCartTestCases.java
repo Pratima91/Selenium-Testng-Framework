6f687bb8392b77334d728ca81e343b4b02efff6e
Pratima91 <pratima.vairagade@gmail.com>
2024-11-15 15:35:03 +0530

    Added New Changes

--- src/test/java/TestCases/AddToCartTestCases.java
+++ src/test/java/TestCases/AddToCartTestCases.java
@@ -4,17 +4,21 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import Resources.BaseClass;
 
-
-public class AddToCartTestCases extends BaseClass {
+public class AddToCartTestCases  {
 
 	@Test
-	public void verifyProductPriceBeforeAndAfter() throws IOException {
+	public void VerifyProductPriceBeforeAndAfter() throws IOException {
+		
+		WebDriver driver = new ChromeDriver();
 
-		initializeDriver();
+		//initializeDriver(); // This driver have scope
 
 		driver.get("https://naveenautomationlabs.com/opencart/");
 
@@ -22,68 +26,84 @@ public class AddToCartTestCases extends BaseClass {
 
 		driver.findElement(By.xpath("//button[@class='btn btn-default btn-lg']")).click();
 
-		
-	    String iphoneCost=driver.findElement(By.xpath("//p[@class='price']")).getText();
-		
+		String iphoneCost = driver.findElement(By.xpath("//p[@class='price']")).getText();
+
 		System.out.println(iphoneCost);
-		
-		String iphoneArray[]=iphoneCost.split(" ");
-		
+
+		String iphoneArray[] = iphoneCost.split(" ");
+
 		System.out.println(Arrays.toString(iphoneArray));
-		
+
 		String iphoneArrayFE = iphoneArray[0];
+
 		System.out.println(iphoneArrayFE);
-		
-		String iphoneCostWithnumber =iphoneArrayFE.replaceAll("[^\\d.]", "");
-		System.out.println(iphoneCostWithnumber);
-		
-		double finaliphoneCost=  Double.parseDouble(iphoneCostWithnumber);
-		System.out.println(finaliphoneCost);
-		
-		//Hw -- 0 -dont remove 0.
-		
-		//addtocart.click();
-		
+
+		String iphoneCostWithNumber = iphoneArrayFE.replaceAll("[^\\d.]", "");
+
+		System.out.println(iphoneCostWithNumber);
+
+		double finalIphoneCost = Double.parseDouble(iphoneCostWithNumber);
+
+		System.out.println(finalIphoneCost);
+
+		// System.out.println(finalIphoneCost);)
+
+		driver.findElement(By.xpath("(//i[@class='fa fa-shopping-cart'])[3]")).click();
+
 		driver.findElement(By.xpath("//input[@placeholder='Search']")).clear();
-		
+
 		driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("samsung");
 
+		driver.findElement(By.xpath("//button[@class='btn btn-default btn-lg']")).click();
+
+		String SamsungCost = driver.findElement(By.xpath("(//p[@class='price'])[2]")).getText();
+
+		System.out.println(SamsungCost);
+
+		String SamsungArray[] = SamsungCost.split(" ");
+
+		System.out.println(Arrays.toString(SamsungArray));
+
+		String SamsungArrayFE = SamsungArray[0];
+
+		System.out.println(SamsungArrayFE);
+
+		String SamsungCostWithNumber = SamsungArrayFE.replaceAll("[^\\d.]", "");
+
+		System.out.println(SamsungCostWithNumber);
+
+		double finalSamsungCost = Double.parseDouble(SamsungCostWithNumber);
+
+		System.out.println(finalSamsungCost);
+
+		driver.findElement(By.xpath("(//i[@class='fa fa-shopping-cart'])[5]")).click();
+
+		double d = finalIphoneCost + finalSamsungCost;
+
+		System.out.println(d);
+
+		String t = "$" + d;
+
+		System.out.println(t);
+
+		//WebElement p = driver.findElement(By.xpath("//span[@id='cart-total']"));
 		
-		
-		
-		 String iphoneCost=driver.findElement(By.xpath("(//p[@class='price'])[2]")).getText();
-			
-			System.out.println(iphoneCost);
-			
-			String iphoneArray[]=iphoneCost.split(" ");
-			
-			System.out.println(Arrays.toString(iphoneArray));
-			
-			String iphoneArrayFE = iphoneArray[0];
-			System.out.println(iphoneArrayFE);
-			
-			String iphoneCostWithnumber =iphoneArrayFE.replaceAll("[^\\d.]", "");
-			System.out.println(iphoneCostWithnumber);
-			
-			double finaliphoneCost=  Double.parseDouble(iphoneCostWithnumber);
-			System.out.println(finalsamsungCost);
-		
-			
-			//Add to cart.click();
-			
-			double c= finalCot+foinalsmsanung ; 365.19
-			//connvet double -->String  -- ad doll
-			
-			"$"+c;  //$365.19
-			
-			
-			
-			//captureThTotalvalufromcart  - $365.19
-			
-			
-			
-		
-		
+		WebElement p = driver.findElement(By.xpath("(//i[@class='fa fa-shopping-cart'])[2]"));
+
+		String TotalText = p.getText();
+
+		System.out.println(TotalText);
+
+		if (t.equals(TotalText)) {
+
+			System.out.println("The Amount is matching successfully");
+
+		} else {
+
+			System.out.println("The Amount is not matching");
+
+		} 
+
 	}
 
 }
